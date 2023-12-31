616b28470f8d9e8447786cfca9d618059c963a8b
Ruturaj2003 <ruturajhb2003@gmail.com>
2023-12-31 18:41:29 +0530

    Fetch Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 const url = 'https://course-api.com/react-tabs-project';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  // Current Item
+  const fetchJobs = async () => {
+    const resp = await fetch(url);
+    const newJobs = await resp.json();
+    // After React 18 it is fine to change the sequence of the following ,as it implements something called batch bashing which basically renders them at the same time
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+  console.log(jobs);
+  useEffect(() => {
+    // fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+
   return <h2>Tabs Starter</h2>;
 };
 export default App;
